de88fc287c2c6d229d496edf828c65440a02e5ca
Younes JAAIDI <yjaaidi@gmail.com>
2019-06-27 08:25:04 +0200

    🚧 add specs for `AutoUnsubscribe`.

---
+++ tests/auto-unsubscribe.spec.ts
@@ -0,0 +1,7 @@
+describe('AutoUnsubscribe', () => {
+
+    it.todo('🚧 should unsubscribe from observable when component is destroyed');
+
+    it.todo('🚧 should unsubscribe from observable when observable is replaced by another');
+
+});
