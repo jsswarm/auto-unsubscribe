8fc89c255bb2ca2873b0eb1973d016b641162a11
Younes JAAIDI <yjaaidi@gmail.com>
2019-06-27 08:45:39 +0200

    🚧 add specs for `AutoUnsubscribe`.

--- tests/auto-unsubscribe.spec.ts
+++ tests/auto-unsubscribe.spec.ts
@@ -1,6 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AutoUnsubscribe } from '../src/auto-unsubscribe';
+
+function createTestComponent() {
+
+    const tearDownSpy = jest.fn();
+
+    @Component({
+        template: ''
+    })
+    class TestComponent implements OnInit {
+
+        @AutoUnsubscribe()
+        data$ = new Observable(() => {
+            return tearDownSpy;
+        });
+
+        ngOnInit() {
+            this.data$.subscribe();
+        }
+
+    }
+
+    return {
+        tearDownSpy,
+        TestComponent
+    };
+
+}
+
 describe('AutoUnsubscribe', () => {
 
-    it.todo('🚧 should unsubscribe from observable when component is destroyed');
+    xit('🚧 should unsubscribe from observable when component is destroyed', () => {
+
+        const {TestComponent, tearDownSpy} = createTestComponent();
+
+        // call TestComponent.ngOnInit()
+        // compile component with IVy.
+        // check tearDownSpy is not called yet.
+        // trigger component onDestroy.
+        // check tearDownSpy is now called.
+
+        throw new Error('🚧 work in progress!');
+
+    });
 
     it.todo('🚧 should unsubscribe from observable when observable is replaced by another');
 
